9a4657892a35be1469086bfc11790cdd44ad5aaf
Evgenii Olshanskii <Evgenii.Olshanskii@telekom.com>
2026-03-04 14:33:56 +0200

    feat: restructure SQL scripts for user management and privileges

--- backend/f1-interactive_db/sql-scripts/02_insert_data.sql
+++ backend/f1-interactive_db/sql-scripts/02_insert_data.sql
@@ -1,3 +1,4 @@
 \connect __DATABASE_NAME__
 
+-- Insert initial admin user (password placeholder will be substituted)
 INSERT INTO __DATABASE_SCHEMA__.users (name, password, roles) values ('admin', '__ADMIN_PWD__', 'ADMIN');
